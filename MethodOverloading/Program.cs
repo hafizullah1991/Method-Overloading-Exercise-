a5f3c7bddf80e24cda857a7ad3857f1265a6fc27
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-18 09:57:49 -0800

    exercise1 is done

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace MethodOverloading
+﻿using System.Runtime.CompilerServices;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int sum = Add(5 , 5);
+            Console.WriteLine(sum);
+
+            Console.WriteLine(Add(4.2, 4.1));
+
+            Console.WriteLine(Add(3, 2 ,true));
+        }
+        public static int Add(int x , int y)
+        {
+            return x + y;
+        }
+
+        public static double Add(double x , double y)
+        {
+            return x + y;
+        }
+        public static string Add(int x, int y, bool Plus)
+
+        {
+
+            int sum= x+ y;
+           
+            if (Plus)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
         }
     }
 }
--- MethodOverloading/obj/Release/net6.0/MethodOverloading.AssemblyInfo.cs
+++ MethodOverloading/obj/Release/net6.0/MethodOverloading.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodOverloading")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eddcfa4da02f5351bf45b7a3c534caeddb2b5239")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodOverloading")]

a44fc4ece26d49198c698086343a321b5ff6ab1e
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-18 11:45:12 -0800

    extra options added

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,31 +6,43 @@ namespace MethodOverloading
     {
         static void Main(string[] args)
         {
-            int sum = Add(5 , 5);
+            int sum = Add(5, 5);
             Console.WriteLine(sum);
 
             Console.WriteLine(Add(4.2, 4.1));
 
-            Console.WriteLine(Add(3, 2 ,true));
-        }
-        public static int Add(int x , int y)
-        {
-            return x + y;
-        }
+            Console.WriteLine("enter true or false: ");
+            bool userTrueFalse = bool.Parse(Console.ReadLine());
+            Console.WriteLine(Add(3, 7, userTrueFalse));
+;  
 
-        public static double Add(double x , double y)
-        {
-            return x + y;
-        }
-        public static string Add(int x, int y, bool Plus)
-
-        {
-
-            int sum= x+ y;
            
-            if (Plus)
+
+        }
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static string Add(int x, int y, bool isDollar)
+
+        {
+            int sum= x + y;
+            if(isDollar)
             {
-                return $"{sum} dollars";
+                if(sum<10)
+                {
+                    return  $"{sum} dollar";
+                }
+                else
+                {
+                    return $"{sum} dollars";
+                }
+                
             }
             else
             {
